b290a0ccad41ceb04f6c6ac44b436749e373bbb8
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-05-15 19:30:47 +0700

    2.Set up project and 3 necessary packages

---
+++ ecommerce/src/app.js
@@ -0,0 +1,22 @@
+const morgan = require("morgan");
+const helmet = require("helmet");
+const express = require("express");
+const compression = require("compression");
+const app = express();
+
+// init middleware
+app.use(morgan("combined"));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init routes
+app.get("/", (req, res) => {
+  const strCompress = "Hello World";
+  return res
+    .status(200)
+    .json({ message: "Hello World", metadata: strCompress.repeat(1000) });
+});
+
+module.exports = app;
